547f2b46838ea5c8a8980b1ff1eb7d3771d49844
czartur <arturalves.ca@gmail.com>
2020-11-08 00:28:40 -0300

    first chart

--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 import { Header } from "react-native-elements";
-
+import { LineChart } from "react-native-chart-kit";
 import { Feather } from "@expo/vector-icons";
 import cor from "../constants/colors";
 
@@ -19,10 +19,51 @@ const DataScreen = (props) => {
             <Feather name="menu" color="#fff" size={20} />
           </TouchableOpacity>
         }
-        centerComponent={{ text: "Estatísticas", style: { color: "#fff", fontFamily: 'sofia-pro' } }}
+        centerComponent={{
+          text: "Estatísticas",
+          style: { color: "#fff", fontFamily: "sofia-pro" },
+        }}
       />
       <View style={styles.centeredView}></View>
-      
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        style={{ marginVertical: 8, borderRadius: 16 }}
+      />
     </View>
   );
 };
