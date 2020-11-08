677e82e4c683ce9dc5b71c5897a184f79d7676d7
srmacario <sergio.macario@imejunior.com.br>
2020-11-08 10:56:49 -0300

    sensores ready

--- screens/SensoresScreen.js
+++ screens/SensoresScreen.js
@@ -31,8 +31,8 @@ const SensoresScreen = (props) => {
       <View style={styles.centeredView}>
         <View style={styles.box}>
           <View style={(styles.image, styles.dummy)}>
-              <Avatar.Image source={require("../assets/irrig_1.png")} size={65}/>
-            <Text style={{ alignSelf: "center", marginTop: 50, fontFamily: "sofia-pro" }}>
+              <Avatar.Image style = {styles.avatar} source={require("../assets/irrig_1.png")} size={65}/>
+            <Text style={styles.name}>
               Sensor A
             </Text>
             <Switch style = {styles.switch}
@@ -59,53 +59,91 @@ const SensoresScreen = (props) => {
             <Text style={styles.percentage}>80% </Text>
           </View>
         </View>
+        <View style={styles.box}>
+          <View style={(styles.image, styles.dummy)}>
+              <Avatar.Image style = {styles.avatar} source={require("../assets/irrig_1.png")} size={65}/>
+            <Text style={styles.name}>
+              Sensor B
+            </Text>
+            <Switch style = {styles.switch}
+              trackColor={{ false: "#767577", true: cor.verde }}
+              thumbColor={isEnabled ? cor.azul : "#f4f3f4"}
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+          <View style={(styles.image, styles.dummy)}>
+            <Text style={styles.description}>Umidade</Text>
+            <Image
+              source={require("../assets/water_1.png")}
+              style={styles.icons}
+            />
+            <Text style={styles.percentage}>55% </Text>
+          </View>
+          <View style={styles.dummy}>
+            <Text style={styles.description}>Bateria</Text>
+            <Image
+              source={require("../assets/battery.png")}
+              style={styles.icons}
+            />
+            <Text style={styles.percentage}>80% </Text>
+          </View>
+        </View>
+        <Image style = {styles.button}
+          source={require('../assets/add.png')}
+        />
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  avatar:{
-    flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-  },
-  switch:{
-    marginTop: 10,
-    alignSelf: "center"
-  },
-  dummy: {
-    borderWidth: 1,
-    borderColor: "black",
-    width: 100,
-  },
-  centeredView: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: cor.azul,
-  },
   menu: {
     marginLeft: 10,
     width: "100%",
   },
+  centeredView: {
+    flex: 1,
+    backgroundColor: cor.azul,
+  },
+  dummy: {
+    borderWidth: 1,
+    borderColor: "white",
+    width: 100,
+  },
   box: {
     justifyContent: "space-evenly",
+    alignSelf: "center",
     flexDirection: "row",
     marginTop: 10,
-    height: 200,
+    height: 150,
     width: "90%",
     backgroundColor: "#fff",
     borderWidth: 1,
     borderRadius: 10,
     borderColor: "#fff",
   },
+  avatar:{
+    alignSelf: "center",
+    marginTop: 12,
+  },
+  name: {
+    alignSelf: "center",
+    marginTop: 10,
+    fontFamily: "sofia-pro"
+  },
+  switch:{
+    marginTop: 5,
+    alignSelf: "center",
+    marginRight: -5
+  },
   image: {
-    marginTop: 30,
+    marginTop: 0,
     marginLeft: 30,
   },
   description:{
     fontFamily: "sofia-pro",
-    marginTop: 30,
+    marginTop: 10,
     textAlign: "center",
   },
   icons:{
@@ -120,9 +158,11 @@ const styles = StyleSheet.create({
     marginTop: 10,
     textAlign: "center",
   },
-  moisture: {},
-  name: {
-    fontFamily: "sofia-pro",
-  },
+  button:{
+    height: "15%",
+    width: "15%",
+    resizeMode: "contain",
+    alignSelf: "center",
+  }
 });
 export default SensoresScreen;
