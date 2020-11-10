3d47904563854693300e2d7f64415553104a9d52
srmacario <sergio.macario@imejunior.com.br>
2020-11-09 23:06:18 -0300

    graph

--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { View, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 import { Header } from "react-native-elements";
-import { LineChart, Grid, YAxis } from "react-native-svg-charts";
+import { LineChart, Grid, YAxis, XAxis } from "react-native-svg-charts";
 import { Feather } from "@expo/vector-icons";
 import cor from "../constants/colors";
 
 const DataScreen = (props) => {
-  const data1 = [
-    0,
-    10,
-    20,
-    30,
-    40,
-    50,
-    60,
-    70,
-    80,
-    90,
+  const data0 = [
     100,
+    90,
+    80,
+    70,
+    60,
+    50,
+    40,
+    30,
+    20,
+    10,
+    0
+  ];
+  const data1 = [
+    78,
+    73,
+    61,
+    58,
+    56,
+    55,
   ];
   const data2 = [
-    40,
     40,
     40,
     40,
@@ -43,7 +50,6 @@ const DataScreen = (props) => {
     60,
     60,
     60,
-    60,
   ];
   const data4 = [
     80,
@@ -56,12 +62,27 @@ const DataScreen = (props) => {
     80,
     80,
     80,
-    80,
+  ];
+  const data5 = [
+    77.5,
+    72,
+    67,
+    62,
+    57,
+    52,
+    47,
+    42,
+    37,
+    32,
   ];
   const data = [
     {
+      data: data0,
+      svg: { stroke: "black" },
+    },
+    {
       data: data1,
-      svg: { stroke: cor.azul },
+      svg: { stroke: "white" },
     },
     {
       data: data2,
@@ -75,9 +96,13 @@ const DataScreen = (props) => {
       data: data4,
       svg: { stroke: "red" },
     },
+    {
+      data: data5,
+      svg: { stroke: "yellow" },
+    },
   ];
   const verticalContent = { top: 10, bottom: 10 };
-  const axesSvg = { fontSize: 10, fill: "grey" };
+  const axesSvg = { fontSize: 10, fill: cor.branco };
   return (
     <View style={{ flex: 1 }}>
       <Header
@@ -103,16 +128,18 @@ const DataScreen = (props) => {
         <View
           style={styles.chart}
         >
-          <YAxis data={data1} contentInset={verticalContent}  numberOfTicks={10} svg={axesSvg} />
+          <YAxis data={data0} contentInset={verticalContent} numberOfTicks={10} svg={axesSvg} />
           <LineChart
-            style={{ flex: 1, height: "100%", width: "90%" }}
+            style={styles.graph}
             data={data}
             //svg={{ stroke: "rgb(134, 65, 244)" }}
-            contentInset={{ top: 20, bottom: 20 }}
+            contentInset={{ top: 20, bottom: 20, right: -30}}
           >
             <Grid />
             
           </LineChart>
+          <XAxis data={data0} contentInset={verticalContent} svg={axesSvg} />
+          
         </View>
         
         <View style={styles.info}>
@@ -128,17 +155,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     backgroundColor: cor.azul,
-  },  
+  },
   menu: {
     marginLeft: 10,
     width: "100%",
   },
+  graph: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+  },
   chart: {
     height: "45%",
-    width: "95%",
-    marginLeft: 10,
+    width: "90%",
+    marginLeft: 30,
     flexDirection: "row",
-    backgroundColor: "#fff"
+    backgroundColor: "black"
   },
   topmenu:{
     alignSelf: "center",
